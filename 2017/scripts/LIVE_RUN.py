eca40fadd5b1c5ea3dbf9b940660307ec5733689
jmk <bob.harper87@gmail.com>
2017-03-10 12:35:58 +0000

    float: tranfers and total votes

--- 2017/scripts/LIVE_RUN.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-subprocess.call("all-constituencies-create-info-json.py", shell=True)
-subprocess.call("all-counts-create-json.py", shell=True)
-subprocess.call("all-counts-find-elected.py", shell=True)
-subprocess.call("d3-find-elected.py", shell=True)
-subprocess.call("all-counts-party-transfers.py", shell=True)
-subprocess.call("all-counts-find-candidate-firsts.py", shell=True)
